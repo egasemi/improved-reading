6c4095e84f4b07ee3f5eeaedccb8a5aada601c7d
egasemi <emiliano.arregui@gmail.com>
2024-05-20 16:48:39 -0300

    fix align logos

--- components/landing/Logos.vue
+++ components/landing/Logos.vue
@@ -5,14 +5,17 @@ const props = defineProps(['logos', 'title'])
 <template>
   <div class="mt-24">
     <h2 class="text-center text-3xl lg:text-4xl text-orange font-bold">{{props.title}}</h2>
-    <div
-      class="flex flex-wrap gap-8 items-top justify-center mt-10"
-    >
-      <div v-for="logo in props.logos" class="w-full md:w-1/2 lg:w-1/5">
-        <h1 v-if="logo.title" class="text-md lg:text-xl lg:tracking-tight text-orange font-bold text-center">{{logo.title}}</h1>
+    <div class="flex flex-col md:flex-row gap-8 items-top justify-center mt-10">
+      <div v-for="logo in props.logos" class="flex flex-col md:w-1/2 lg:w-1/5 mb-5 md:justify-center">
+        <h1 v-if="logo.title" class="text-xl lg:tracking-tight text-orange font-bold text-center">{{logo.title}}</h1>
         <p v-if="logo.subtitle" class="text-center">{{logo.subtitle}}</p>
-        <NuxtImg :src="'img/'+logo.img" sizes="250px"/>
+        <img :src="'img/'+logo.img" style="margin-top: auto;"/>
       </div>
     </div>
   </div>
 </template>
+<style>
+.fixed-height {
+  height: 100px;
+}
+</style>
