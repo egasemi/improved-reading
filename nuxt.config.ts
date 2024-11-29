bcd8897a6e1130d2c234a2426bb1ada67240b6df
egasemi <emiliano.arregui@gmail.com>
2024-11-29 14:42:44 -0300

    improve og cards

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -20,7 +20,14 @@ export default defineNuxtConfig({
   },
   app: {
     head: {
-      title: "Improved Reading"
-    }
+      title: "Improved Reading",
+      meta: [
+        { hid: 'og:title', property: 'og:title', content: 'Improved Reading' },
+        { hid: 'og:description', property: 'og:description', content: 'Lectura Rápida y Eficaz para profesionales por la empresa líder del mercado .' },
+        { hid: 'og:image', property: 'og:image', content: 'https://improved-reading.com.ar/img/logo.png' },
+        { hid: 'og:url', property: 'og:url', content: 'https://improved-reading.com.ar' },
+        { hid: 'og:type', property: 'og:type', content: 'website' }
+      ]
+    },
   }
 });
